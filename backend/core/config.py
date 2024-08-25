11229443ac03abf1d9616037c470076aedf7ca50
Lev <lafitel31@gmail.com>
2024-08-25 11:41:50 +1100

    some fix

--- backend/core/config.py
+++ backend/core/config.py
@@ -17,8 +17,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=False,
-        env_nested_delimiter="__"
-        env_prefix="FASTAPI__"
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__",
     )
     run: RunConfig = RunConfig()
     db: DataBaseConfig
