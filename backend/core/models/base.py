deb730bd08922dcc3e6bac018fd208af511b7450
Lev <lafitel31@gmail.com>
2024-08-28 17:27:14 +1100

    add id int pk in base class

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import declared_attr
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 from utils import camel_case_to_snake_case
 
@@ -10,3 +12,5 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
