f64b2f5717a492423ad5ca0144a1267006432281
Lev <lafitel31@gmail.com>
2024-08-28 18:50:13 +1100

    add AccessToken in migration fov

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,9 +1,10 @@
 __all__ = (
     "Base",
     "User",
+    "AccessToken",
     "db_helper",
 )
 
 from .base import Base
-from auth.models import User
+from auth.models import User, AccessToken
 from .db_helper import db_helper
