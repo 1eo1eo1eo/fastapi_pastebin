37256d134ca0cc0646629a44cafbb9a21b6164d3
Lev <lafitel31@gmail.com>
2024-08-28 18:28:25 +1100

    create UserIdType var

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,17 +1,25 @@
 from typing import TYPE_CHECKING
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase
+    SQLAlchemyUserDatabase,
 )
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable
 
 from core.models import Base
+from types.user_id import UserIdType
+
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, SQLAlchemyBaseUserTable[int]):
+
+class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
     def get_db(cls, session: AsyncSession):
         return SQLAlchemyUserDatabase(session, User)
+    
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
+    pass
     
