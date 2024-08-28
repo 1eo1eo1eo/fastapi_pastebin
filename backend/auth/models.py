207660d514dfceb6e3bc8d3541a2321eff97505a
Lev <lafitel31@gmail.com>
2024-08-28 17:27:51 +1100

    remove mixin IdIntPk

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,8 +1,7 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
 
 from core.models import Base
-from core.models.mixins.id_int_pk import IdIntPkMixin
 
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, SQLAlchemyBaseUserTable[int]):
     pass
