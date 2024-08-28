723cacfa50db0baa4342587adc1fb864ef548019
Lev <lafitel31@gmail.com>
2024-08-28 18:36:38 +1100

    create AccessToken model

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -3,7 +3,12 @@ from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
-from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyBaseAccessTokenTable,
+    SQLAlchemyAccessTokenDatabase
+)
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 from types.user_id import UserIdType
@@ -16,10 +21,18 @@ if TYPE_CHECKING:
 class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
-    def get_db(cls, session: AsyncSession):
+    def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, User)
     
 
 class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
-    pass
+    user_id: Mapped = mapped_column(
+        Integer,
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False
+    )
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
     
