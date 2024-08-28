e5096961bcba6ce0d39d8a635b7b2678e14aa886
Lev <lafitel31@gmail.com>
2024-08-28 17:38:53 +1100

    add method for user database

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,7 +1,17 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase
+)
 
 from core.models import Base
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
+    
