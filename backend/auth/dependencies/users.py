b907289b30c6347fcb669f6e6ead97cad4b3f25e
Lev <lafitel31@gmail.com>
2024-08-29 12:41:47 +1100

    small fix

--- backend/auth/dependencies/users.py
+++ backend/auth/dependencies/users.py
@@ -4,16 +4,17 @@ from fastapi import Depends
 
 from core.models import (
     db_helper,
-    User
+    User,
 )
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
+
 async def get_users_db(
     session: Annotated[
         "AsyncSession",
         Depends(db_helper.session_getter),
-]
+    ]
 ):
     yield User.get_db(session=session)
