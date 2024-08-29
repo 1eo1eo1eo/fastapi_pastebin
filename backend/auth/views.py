15a2c0997827f5f9f3343a4c0b44b470d7e379f7
Lev <lafitel31@gmail.com>
2024-08-29 12:35:07 +1100

    create fastapi_users routers helper

---
+++ backend/auth/views.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from .models import User
+from .backend import authentication_backend
+from .user_manager import get_user_manager
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
