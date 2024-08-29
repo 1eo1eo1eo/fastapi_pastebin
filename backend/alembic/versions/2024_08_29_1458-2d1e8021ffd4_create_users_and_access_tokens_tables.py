6cd9492ac4f4e6d67e1deb216d77b339278401d0
Lev <lafitel31@gmail.com>
2024-08-29 15:00:47 +1100

    create users and access token tables

--- backend/alembic/versions/2024_08_29_1458-2d1e8021ffd4_create_users_and_access_tokens_tables.py
+++ backend/alembic/versions/2024_08_29_1458-2d1e8021ffd4_create_users_and_access_tokens_tables.py
@@ -1,19 +1,20 @@
-"""create users table
+"""create users and access tokens tables
 
-Revision ID: 68c90d43c22f
+Revision ID: 2d1e8021ffd4
 Revises: 
-Create Date: 2024-08-28 17:24:41.124372
+Create Date: 2024-08-29 14:58:26.002187
 
 """
 
 from typing import Sequence, Union
 
 from alembic import op
+import fastapi_users_db_sqlalchemy
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "68c90d43c22f"
+revision: str = "2d1e8021ffd4"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -31,8 +32,28 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+    op.create_table(
+        "access_tokens",
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"], ondelete="cascade"),
+        sa.PrimaryKeyConstraint("token"),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
 
 
 def downgrade() -> None:
+    op.drop_index(op.f("ix_access_tokens_created_at"), table_name="access_tokens")
+    op.drop_table("access_tokens")
     op.drop_index(op.f("ix_users_email"), table_name="users")
     op.drop_table("users")
