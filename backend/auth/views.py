a3f7ac2a52bb7dfb2535701036f75dd04d11e0a7
Lev <lafitel31@gmail.com>
2024-08-29 17:22:54 +1100

    add httpBearer

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from .dependencies.router_helper import fastapi_users
 from .dependencies.backend import authentication_backend
 from .dependencies.schemas import UserRead, UserCreate, UserUpdate
 
 
+http_bearer = HTTPBearer(auto_error=False)
+
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"],
+    dependencies=[Depends(http_bearer)],
 )
 
 # /login
