57c883d67796e15e9ab72ee6f179744102deec5c
Lev <lafitel31@gmail.com>
2024-08-29 13:01:24 +1100

    small import fix custom auth

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,7 +8,7 @@ from users.schemas import User as response_user
 from users.dependencies import product_by_id
 
 from core.models import db_helper
-from core.models import User
+from .models import User
 
 
 router = APIRouter(
@@ -16,11 +16,11 @@ router = APIRouter(
     tags=["Users"],
 )
 
+
 @router.get("/{user_id}", response_model=response_user)
-async def get_user(
-    user: User = Depends(product_by_id)
-):
+async def get_user(user: User = Depends(product_by_id)):
     return user
+
 
 @router.get("", response_model=list[response_user])
 async def get_users(
@@ -32,13 +32,14 @@ async def get_users(
     users = await users_crud.get_all_users(session=session)
     return users
 
+
 @router.post("", response_model=response_user, status_code=status.HTTP_201_CREATED)
 async def create_user(
     user_create: UserCreate,
     session: Annotated[
         AsyncSession,
         Depends(db_helper.session_getter),
-    ]
+    ],
 ):
     user = await users_crud.create_user(
         session=session,
@@ -46,12 +47,10 @@ async def create_user(
     )
     return user
 
+
 @router.put("/{user_id}", response_model=response_user)
 async def update_user(
-    user: Annotated[
-        User,
-        Depends(product_by_id)
-    ],
+    user: Annotated[User, Depends(product_by_id)],
     session: Annotated[
         AsyncSession,
         Depends(db_helper.session_getter),
@@ -64,12 +63,10 @@ async def update_user(
         user_update=user_update,
     )
 
+
 @router.patch("/{user_id}", response_model=response_user)
 async def update_user_partial(
-    user: Annotated[
-        User,
-        Depends(product_by_id)
-    ],
+    user: Annotated[User, Depends(product_by_id)],
     session: Annotated[
         AsyncSession,
         Depends(db_helper.session_getter),
@@ -81,6 +78,7 @@ async def update_user_partial(
         user=user,
         user_update_partial=user_update_partial,
     )
+
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
