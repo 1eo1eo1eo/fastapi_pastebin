fa2926211827e4cc620d1937aa5987efee0acc07
Lev <lafitel31@gmail.com>
2024-08-29 11:53:57 +1100

    move transport file

--- backend/auth/backend.py
+++ backend/auth/backend.py
@@ -1,6 +1,6 @@
 from fastapi_users.authentication import AuthenticationBackend
 
-from .transport import bearer_transport
+from core.authentication.transport import bearer_transport
 from .strategy import get_database_strategy
 
 
