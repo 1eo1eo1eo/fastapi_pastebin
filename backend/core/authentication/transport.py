e075d68654be999ba6987871fff958a1db5de066
Lev <lafitel31@gmail.com>
2024-08-29 13:32:56 +1100

    update bearer transport token url

--- backend/core/authentication/transport.py
+++ backend/core/authentication/transport.py
@@ -1,6 +1,3 @@
 from fastapi_users.authentication import BearerTransport
 
-bearer_transport = BearerTransport(
-    # TODO: update url
-    tokenUrl="auth/jwt/login"
-)
+bearer_transport = BearerTransport(tokenUrl="auth/login")
