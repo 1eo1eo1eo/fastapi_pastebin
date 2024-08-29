21b7d0a2fa9f0873376da17db54902d15a76b78a
Lev <lafitel31@gmail.com>
2024-08-29 15:01:19 +1100

    add mixin IdIntPkMixin to users table

--- backend/auth/dependencies/models.py
+++ backend/auth/dependencies/models.py
@@ -11,13 +11,14 @@ from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
+from core.models.mixins.id_int_pk import IdIntPkMixin
 
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, SQLAlchemyBaseUserTable[int]):
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
@@ -26,7 +27,9 @@ class User(Base, SQLAlchemyBaseUserTable[int]):
 
 class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int]):
     user_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("users.id", ondelete="cascade"), nullable=False
+        Integer,
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False,
     )
 
     @classmethod
