36a48e60ae75cb99f12bbb2dd6680b420549d00e
Lev <lafitel31@gmail.com>
2024-08-29 11:26:52 +1100

    create authentication backend

---
+++ backend/auth/backend.py
@@ -0,0 +1,11 @@
+from fastapi_users.authentication import AuthenticationBackend
+
+from .transport import bearer_transport
+from .strategy import get_database_strategy
+
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
