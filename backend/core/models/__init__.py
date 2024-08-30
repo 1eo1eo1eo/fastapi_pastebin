60761823203446935931d2c1e5543a13ca963337
Lev <lafitel31@gmail.com>
2024-08-30 11:56:21 +1100

    add Message model to alembic fov

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,11 +1,13 @@
 __all__ = (
+    "db_helper",
     "Base",
     "User",
     "AccessToken",
-    "db_helper",
+    "Message",
 )
 
 
+from .db_helper import db_helper
 from .base import Base
 from auth.dependencies.models import User, AccessToken
-from .db_helper import db_helper
+from api.api_v1.models import Message
