022c8b053b8519f2d81342dda31b1362b9d13926
Lev <lafitel31@gmail.com>
2024-08-30 14:10:59 +1100

    add cruds: get_message_by_id, get_message_by_sid, create_message

---
+++ backend/api/api_v1/crud.py
@@ -0,0 +1,35 @@
+from typing import Annotated, TYPE_CHECKING, Sequence
+
+from sqlalchemy import select
+
+from .schemas import MessageCreate
+from .models import Message
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_message_by_id(
+    session: "AsyncSession",
+    message_id: int,
+) -> Message | None:
+    return await session.get(Message, message_id)
+
+
+async def get_message_by_sid(
+    session: "AsyncSession",
+    message_sid: str,
+) -> Sequence[Message] | None:
+    stmt = select(Message).where(Message.sid == message_sid)
+    result = await session.scalars(stmt)
+    return result.all()
+
+
+async def create_message(
+    session: "AsyncSession",
+    message_create: MessageCreate,
+) -> Message:
+    message = Message(**message_create.model_dump())
+    session.add(message)
+    await session.commit()
+    return message
